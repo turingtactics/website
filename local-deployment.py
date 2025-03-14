26a2280afc6f4eb346522b13c1f1ba8025a54d8b
Stanny <stannygoffin@live.nl>
2025-03-14 22:08:16 +0100

    Upgraded style

---
+++ local-deployment.py
@@ -0,0 +1,18 @@
+import webbrowser
+url = r'C:\Users\Stann\PycharmProjects\website\index.html'
+webbrowser.open(url, new=2)  # open in new tab
+url = r'C:\Users\Stann\PycharmProjects\website\reinforcement-learning.html'
+webbrowser.open(url, new=2)  # open in new tab
+url = r'C:\Users\Stann\PycharmProjects\website\applied-reinforcement-learning.html'
+webbrowser.open(url, new=2)  # open in new tab
+url = r'C:\Users\Stann\PycharmProjects\website\about-me.html'
+webbrowser.open(url, new=2)  # open in new tab
+url = r'C:\Users\Stann\PycharmProjects\website\about-you.html'
+webbrowser.open(url, new=2)  # open in new tab
+
+url = r'C:\Users\Stann\PycharmProjects\website\page1.html'
+webbrowser.open(url, new=2)  # open in new tab
+url = r'C:\Users\Stann\PycharmProjects\website\page2.html'
+webbrowser.open(url, new=2)  # open in new tab
+url = r'C:\Users\Stann\PycharmProjects\website\page3.html'
+webbrowser.open(url, new=2)  # open in new tab
