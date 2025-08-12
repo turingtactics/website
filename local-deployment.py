a0e3a7bce72278de9ac71d16b25aa625f813a0a6
Stanny <stannygoffin@live.nl>
2025-08-12 15:02:57 +0200

    Background lighter

--- local-deployment.py
+++ local-deployment.py
@@ -1,18 +1,8 @@
-import webbrowser
-url = r'C:\Users\Stann\PycharmProjects\website\index.html'
-webbrowser.open(url, new=2)  # open in new tab
-url = r'C:\Users\Stann\PycharmProjects\website\reinforcement-learning.html'
-webbrowser.open(url, new=2)  # open in new tab
-url = r'C:\Users\Stann\PycharmProjects\website\applied-reinforcement-learning.html'
-webbrowser.open(url, new=2)  # open in new tab
-url = r'C:\Users\Stann\PycharmProjects\website\about-me.html'
-webbrowser.open(url, new=2)  # open in new tab
-url = r'C:\Users\Stann\PycharmProjects\website\about-you.html'
-webbrowser.open(url, new=2)  # open in new tab
+import http.server, socketserver
 
-url = r'C:\Users\Stann\PycharmProjects\website\page1.html'
-webbrowser.open(url, new=2)  # open in new tab
-url = r'C:\Users\Stann\PycharmProjects\website\page2.html'
-webbrowser.open(url, new=2)  # open in new tab
-url = r'C:\Users\Stann\PycharmProjects\website\page3.html'
-webbrowser.open(url, new=2)  # open in new tab
+PORT = 8000
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print(f"Serving at http://localhost:{PORT}")
+    httpd.serve_forever()
